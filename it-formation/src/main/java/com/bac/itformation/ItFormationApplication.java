1d0dd75e8d526f3113b23c9e301f76c60b1122f1
Alexis Donnez <alexis.donnez62@gmail.com>
2024-02-02 11:26:39 +0100

    commit initial

---
+++ it-formation/src/main/java/com/bac/itformation/ItFormationApplication.java
@@ -0,0 +1,13 @@
+package com.bac.itformation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ItFormationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ItFormationApplication.class, args);
+	}
+
+}
---
+++ it-formation/src/test/java/com/bac/itformation/ItFormationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bac.itformation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ItFormationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
