f9b8ec2ddfc3d239bbd2681dae60a7d6a00a8c9d
Alexis Donnez <alexis.donnez62@gmail.com>
2024-02-13 10:51:07 +0100

    modification models

--- it-formation/src/main/java/com/bac/itformation/repository/CompteRepository.java
+++ it-formation/src/main/java/com/bac/itformation/repository/CompteRepository.java
@@ -2,8 +2,12 @@ package com.bac.itformation.repository;
 
 import com.bac.itformation.model.Compte;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface CompteRepository extends JpaRepository<Compte, Long> {
+
+    Compte findByMotDePasseAndIdentifiant(String motDePasse, String identifiant);
 }
--- it-formation/src/main/java/com/bac/itformation/service/CompteService.java
+++ it-formation/src/main/java/com/bac/itformation/service/CompteService.java
@@ -26,6 +26,10 @@ public class CompteService {
         return compteRepository.findById(id).orElseThrow( () -> new NotFoundException("Compte inconnue !"));
     }
 
+    public Compte findByCompte(Compte compte){
+        return compteRepository.findByMotDePasseAndIdentifiant(compte.getMotDePasse(),compte.getIdentifiant());
+    }
+
     public Compte save(Compte compte) { return compteRepository.save(compte);}
 
 
